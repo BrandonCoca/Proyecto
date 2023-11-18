687943bd19a5b2a3cff868e5a8c47d98c68f4fe7
Brandon Alexander Coca Aguilera <brandon.coca@sistemas.edu.bo>
2023-11-17 20:56:29 -0400

    Se agrego la cadena de conexion

--- Proyecto/Proyecto/Models/Usuario.cs
+++ Proyecto/Proyecto/Models/Usuario.cs
@@ -4,6 +4,7 @@ namespace Proyecto.Models
 {
     public class Usuario
     {
+        public nint Id { get; set; }
         public string? Cuenta { get; set; }
         public string? Nombre { get; set; }
         public string? Password { get; set; }
