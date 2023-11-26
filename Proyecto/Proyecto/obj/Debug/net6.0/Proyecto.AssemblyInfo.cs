3e91c82187b69bd0a8aea3730f71e8236f3f3b16
Brandon Alexander Coca Aguilera <brandon.coca@sistemas.edu.bo>
2023-11-25 21:14:54 -0400

    Cambios

--- Proyecto/Proyecto/obj/Debug/net6.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/Proyecto/obj/Debug/net6.0/Proyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2231f89128d9816a37c4b9e7bf306f7616f359e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
