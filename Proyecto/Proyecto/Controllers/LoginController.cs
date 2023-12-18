7a66f1b2e7398d30121848f85cb34e1f05edec1b
William <lanswzz@gmail.com>
2023-12-18 19:44:07 -0400

    Login

---
+++ Proyecto/Proyecto/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto.Context;
+
+namespace Proyecto.Controllers
+{
+    public class LoginController : Controller
+    {
+        MiContext _miContext;
+        public LoginController(MiContext miContext)
+        {
+            _miContext = miContext;
+        }
+        public IActionResult Index()
+        {
+            // para la pantalla de login
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string correo, string contrasena)
+        {
+            var usuario = await _miContext.Usuarios
+                .Where(x => x.Email == correo && x.Password == contrasena)
+                .FirstOrDefaultAsync();
+            if (usuario == null)// se ha controlado
+            {
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                TempData["LoginError"] = "Cuenta y passgord incorrecto";
+                return Redirect("Index");
+            }
+        }
+
+    }
+}
