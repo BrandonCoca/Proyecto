ee11809e4893eb0e7fc47c29e2854e2f3be19b77
veliz <veliz@LAPTOP-2TRC1NPS>
2023-12-17 21:34:10 -0400

    Modificacion

--- Proyecto/Proyecto/obj/Debug/net6.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/Proyecto/obj/Debug/net6.0/Proyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71b76bdfcdff4ab155bc2ab7bd5617f6a9b413f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
